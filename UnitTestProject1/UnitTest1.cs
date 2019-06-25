1f22f12b9f458cede3b7bbece64db0a498a50915
Nistha Singh <nistha.singh@pwc.com>
2019-06-25 14:30:49 +0530

    Added Test Cases

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static MyFirstReactApp.Controllers.SampleDataController;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int expected = 76;
+            WeatherForecast weatherObj = new WeatherForecast();
+            weatherObj.TemperatureC = 25;
+            int actual = weatherObj.TemperatureF;
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Temperature conversion-Failed ");
+
+
+        }
+    }
+}
+
