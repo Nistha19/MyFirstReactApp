ae502669f1d5ef04efddef84706721a5318f734b
Nistha Singh <nistha.singh@pwc.com>
2019-09-10 12:33:30 +0530

    changes to unit test

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System;
 using static MyFirstReactApp.Controllers.SampleDataController;
 
 namespace UnitTestProject1
@@ -7,6 +7,18 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
+        /*
+        public static int celsiusTemperature = 0;
+        [ClassInitialize]
+        public static void TestClassInitialize(TestContext context)
+        {
+            var celsiusTemperature = Convert.ToInt32(context.Properties["celsiusTemp"]);
+            
+            Console.WriteLine(celsiusTemperature);
+        }
+        */
+        
+
         [TestMethod]
         public void TestMethod1()
         {
@@ -22,7 +34,28 @@ namespace UnitTestProject1
             Assert.AreEqual(expected, actual, "Temperature Conversion Failed-1");
 
 
+            
         }
+        
+     
+        [DataTestMethod]
+        [DataRow(25)]
+        public void TestMethod2(int temp)
+
+        {
+            int expectedTemp = 76;
+
+            WeatherForecast weatherObj = new WeatherForecast
+            {
+                TemperatureC = temp
+            };
+            int actual = weatherObj.TemperatureF;
+
+            //Assert
+            Assert.AreEqual(expectedTemp, actual, "Temperature Conversion Failed-1");
+
+        }
+
     }
 }
 
